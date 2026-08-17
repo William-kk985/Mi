d70af1d73a795ec8d8ee603477b37f4c09495fe0
zhouyuhan <b0000ff666@qq.com>
2026-08-17 20:22:50 +0800

    stage5修改

--- cyberdog_sim/src/cyberdog_simulator/cyberdog_race/include/cyberdog_race/stages/real/stage5_real.hpp
+++ cyberdog_sim/src/cyberdog_simulator/cyberdog_race/include/cyberdog_race/stages/real/stage5_real.hpp
@@ -17,6 +17,7 @@ public:
 
 private:
     enum class State {
+        WAIT_FOR_SENSORS,
         WALK_SECTION_1,
         TURN_LEFT,
         WALK_SECTION_2,
@@ -33,24 +34,37 @@ private:
         DONE
     };
 
-    void begin_walk(State next_state, float distance, float yaw_offset, bool tilted);
+    void begin_walk(
+        State next_state,
+        float distance,
+        float yaw_offset,
+        bool tilted);
+
     bool update_walk();
-    void begin_turn(State next_state, float yaw_offset);
+
+    void begin_turn(
+        State next_state,
+        float yaw_offset);
+
     bool update_turn();
+
     float projected_distance() const;
 
-    State state_{State::WALK_SECTION_1};
+    State state_{State::WAIT_FOR_SENSORS};
     State next_state_{State::WALK_SECTION_1};
 
     float start_yaw_{0.0f};
     float target_yaw_{0.0f};
     float target_distance_{0.0f};
+
     float segment_start_x_{0.0f};
     float segment_start_y_{0.0f};
+
     float desired_roll_{0.0f};
 
     int stable_frames_{0};
     int state_frames_{0};
-    bool done_{false};
-};
 
+    bool done_{false};
+    bool localization_initialized_{false};
+};
